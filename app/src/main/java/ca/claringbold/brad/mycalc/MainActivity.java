793a862ec9d434c549f7e67d057dbb12b472f397
bclaringbold <bclaringbold@gmail.com>
2016-03-11 00:11:28 -0500

    update
    added functionality for add, sub, mult, divide

--- app/src/main/java/ca/claringbold/brad/mycalc/MainActivity.java
+++ app/src/main/java/ca/claringbold/brad/mycalc/MainActivity.java
@@ -12,14 +12,16 @@ public class MainActivity extends AppCompatActivity {
     Button button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
     Button buttondecimal,buttonadd,buttonsubtract,buttonmultiply,buttondivide,buttonequal,buttonclear;
     TextView textoutput;
-    int value1,value2;
-    boolean add,subtract,divide,multiply,decimalpressed;
-
+    double value1, value2;
+    boolean add, subtract, divide, multiply, decimalpressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        value1 = 0;
+        value2 = 0;
 
         button1=(Button) findViewById(R.id.button1);
         button2=(Button) findViewById(R.id.button2);
@@ -108,14 +110,79 @@ public class MainActivity extends AppCompatActivity {
                     decimalpressed=true;
                     textoutput.setText(textoutput.getText()+".");
                 }
-
             }
         });
         buttonclear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 decimalpressed=false;
-                textoutput.setText("");
+                textoutput.setText(null);
+                value1 = 0;
+                value2 = 0;
+            }
+        });
+
+        buttonadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value1=Double.parseDouble(textoutput.getText()+"");
+                add=true;
+                decimalpressed=false;
+                textoutput.setText(null);
+            }
+        });
+
+        buttonsubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value1=Double.parseDouble(textoutput.getText()+"");
+                subtract=true;
+                decimalpressed=false;
+                textoutput.setText(null);
+            }
+        });
+
+        buttonmultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value1=Double.parseDouble(textoutput.getText()+"");
+                multiply=true;
+                decimalpressed=false;
+                textoutput.setText(null);
+            }
+        });
+
+        buttondivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value1=Double.parseDouble(textoutput.getText()+"");
+                divide=true;
+                decimalpressed=false;
+                textoutput.setText(null);
+            }
+        });
+
+        buttonequal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decimalpressed=false;
+                value2=Double.parseDouble(textoutput.getText()+"");
+                if (add==true) {
+                    textoutput.setText(value1+value2+"");
+                    add=false;
+                }
+                if (subtract==true) {
+                    textoutput.setText(value1-value2+"");
+                    subtract=false;
+                }
+                if (multiply==true) {
+                    textoutput.setText(value1*value2+"");
+                    multiply=false;
+                }
+                if (divide==true) {
+                    textoutput.setText(value1/value2+"");
+                    divide=false;
+                }
             }
         });
 
