d6d4b0d1b8da7d7ebc236fef9888d44fc8aa7437
bclaringbold <bclaringbold@gmail.com>
2016-03-11 00:19:06 -0500

    update

--- app/src/main/java/ca/claringbold/brad/mycalc/MainActivity.java
+++ app/src/main/java/ca/claringbold/brad/mycalc/MainActivity.java
@@ -20,9 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        value1 = 0;
-        value2 = 0;
-
         button1=(Button) findViewById(R.id.button1);
         button2=(Button) findViewById(R.id.button2);
         button3=(Button) findViewById(R.id.button3);
